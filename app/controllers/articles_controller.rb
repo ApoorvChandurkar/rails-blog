2d09f6a69fb3ee7de3f131dfb75f8466b7614e65
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-08 19:48:21 +0530

    Full editing feature added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,19 +10,34 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+      @user_article = Article.new
     end
 
     def create
         @user_article = Article.new(params.require(:article).permit(:title, :description)) #rails shortcut
         
-				if @user_article.save
-				  	
-				end
-					
-            redirect_to @user_article
+        if @user_article.save
+          flash[:notice] = "Article created successfully"
+          redirect_to @user_article
+				            
         else
             render 'new'
         end
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+      @updated_article = Article.find(params[:id])
+
+      if @updated_article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Article updated successfully"
+        redirect_to @updated_article
+
+      else
+        render 'edit'
+      end
     end
 end
