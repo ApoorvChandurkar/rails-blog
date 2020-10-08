b3b75380ce08668a849018e2232c757b35b54928
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-08 18:11:30 +0530

    Added functionality of creating new post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,26 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
     def index
         @articles = Article.all
-
         
+    end
+
+    def new
+
+    end
+
+    def create
+        @user_article = Article.new(params.require(:article).permit(:title, :description)) #rails shortcut
+        
+				if @user_article.save
+				  	
+				end
+					
+            redirect_to @user_article
+        else
+            render 'new'
+        end
     end
 end
