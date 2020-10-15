e4f7770d04a1af16d02f14454fde1e8569600ae7
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-15 17:17:44 +0530

    added before_save mathod

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles    #plurality is important
   validates :username, presence: true, 
                         uniqueness: { case_sensitive: false }, 
