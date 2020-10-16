de0a379bd398daeb378c30feccf45b1254452d27
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-16 11:13:00 +0530

    completed user profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(url, alt: user.username, class: "mt-3 rounded shadow mx-auto d-block")
+  end
 end
