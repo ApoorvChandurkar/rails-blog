02632ab61ff81311461fd7d814279938d1e6fa8e
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-16 17:38:05 +0530

    added helper functions to maintain session information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5) #if articles.paginate, everuthing will be posted, so important point
   end
 
@@ -17,19 +16,20 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{@user.username} you have successfully signed up!"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'new'
     end
   end
 
   def edit
-    @user = User.find(params[:id])
+   
   end
 
   def update
-    @user = User.find(params[:id])
+    
 
     if @user.update(user_params)
       flash[:notice] = "Your information was successfully updated"
@@ -44,4 +44,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
     url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(url, alt: user.username, class: "mt-3 rounded shadow mx-auto d-block")
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
