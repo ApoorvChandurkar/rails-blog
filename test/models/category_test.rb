aea588afa94610ea8a520eb8955d5670a7b3eb6b
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-18 13:12:30 +0530

    added more unit tests for model validation

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,8 +2,40 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: "sports")
-    @category.valid?
+
   end
+
+  test "category should be valid" do
+    
+    @category.valid?
+
+  end
+
+  test "name can't be blank" do
+    @category.name = "       "
+    assert_not @category.valid?
+  end
+
+  test "name must be unique" do
+    @category.save
+
+    @new_category = Category.new(name: "sports")
+    assert_not @new_category.valid?
+
+  end
+
+  test "name can't be too long" do
+    
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name can't be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+
+
 end
