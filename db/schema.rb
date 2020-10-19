840cdc5c87302d0544f72ad65e2ea0b882098a36
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-19 14:34:55 +0530

    added many-to-many association from the back-end in models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_071158) do
+ActiveRecord::Schema.define(version: 2020_10_19_084008) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2020_10_18_071158) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "articles_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
